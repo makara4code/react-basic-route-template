be2a15cef3adbb66a746e3b57557ab0227a5be57
makara4code <nuolmakara@gmail.com>
2025-11-08 22:25:21 +0700

    data fetching

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  // http request, I/O: write/read file
+  // offload
+  // main thread(single threaded)
+  const fetchPosts = async () => {
+    // browser native fetch api
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+
+    setPosts(data);
+  };
+
+  // useEffect(callback, dependencyArray)
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div>
       <h1>Home Page Component</h1>
+      <h2>Data Fetching</h2>
+      {/* unordered list */}
+      <ol>
+        {posts.map((post) => {
+          return (
+            <li>
+              <p>ID: {post.id}</p>
+              <p>UserID: {post.userId}</p>
+              <h3>Title: {post.title}</h3>
+              <p>Content: {post.body}</p>
+            </li>
+          );
+        })}
+
+        {/* sigle line return */}
+        {/* {posts.map((item) => (
+          <li>{item.title}</li>
+        ))} */}
+      </ol>
     </div>
   );
 }
